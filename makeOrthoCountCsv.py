3c89cd65ae90f4395d2410abba2ae5725839645d
Colby Witherup <colbywitherup@gmail.com>
2017-11-10 10:50:38 -0600

    fixed loading json dictionary

--- makeOrthoCountCsv.py
+++ makeOrthoCountCsv.py
@@ -8,7 +8,9 @@ import csv
 ###sys.argv[3] is name for output csv file
 
 species_list=[line.split("\t")[0].rstrip("\n") for line in open(sys.argv[1],"r")]
-ortho_dict=json.loads(sys.argv[2])
+with open(sys.argv[2],"r") as f:
+	for line in f:
+		ortho_dict=json.loads(line)
 
 output=open(sys.argv[3],"wb")
 writer = csv.writer(output)
